0ae67d2b6a7881db99b9033669810d64644a5ea9
Sypula Aleksandra <62116044+ASypula@users.noreply.github.com>
2023-10-10 20:32:39 +0200

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup 
+  
+setup( 
+    name='test_module', 
+    version='1.0', 
+    description='A test Python package', 
+    packages=['test_module'], 
+    install_requires=[], 
+) 
